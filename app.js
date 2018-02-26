87c3825780e3637f79d2387a3def4b4f0ff2b012
Glenn Latomme <glenn.latomme@gmail.com>
2018-02-26 09:44:01 +0100

    Making changes for heroku app

--- app.js
+++ app.js
@@ -11,28 +11,9 @@ const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 let
-  mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+  mongoURL = process.env.MONGODB_URI || process.env.MONGO_URL,
   mongoURLLabel = "";
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  const mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-    mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-    mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
-  mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
-}
 
 mongoose.connect(mongoURL, { useMongoClient: true, promiseLibrary: require('bluebird') })
   .then(() =>  console.log('connection succesful'))
