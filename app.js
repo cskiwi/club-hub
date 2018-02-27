e020ef8220bae7b4d90cd99daadc789cc9981646
Glenn Latomme <glenn.latomme@gmail.com>
2018-02-27 15:18:39 +0100

    Cleaning up some code

--- app.js
+++ app.js
@@ -3,23 +3,26 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
-const app = express();
-app.use(cors());
-
 const mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
 
+// Set up app
+const app = express();
+
+// Fetch url from env
 let mongoURL = process.env.MONGODB_URI || process.env.MONGO_URL;
 
-
+// Monogo connection
+mongoose.Promise = require('bluebird');
 mongoose.connect(mongoURL, {useMongoClient: true, promiseLibrary: require('bluebird')})
   .then(() => console.log('connection succesful'))
   .catch((err) => console.error(err));
 
+// Config stuff
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
 // resources
 app.use(express.static(path.join(__dirname, 'dist')));
 
--- server/routes/angular.route.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const routes = require('express').Router();
-
-routes.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
-module.exports = routes;
